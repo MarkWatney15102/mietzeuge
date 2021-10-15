ca50ebb4993f91bdf1d936b8a4c2864cc63568f5
MarkWatney15102 <eikewientjes882@gmail.com>
2021-10-15 13:54:43 +0200

    ADD: Login System

---
+++ components/login/failed.js
@@ -0,0 +1,7 @@
+export default function Failed() {
+    return <>
+        <div class="alert alert-danger hidden notification" role="alert" id="login-failed-info">
+            Benutzername oder Passwort war falsch
+        </div>
+    </>;
+}
--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,54 @@
-//import { login, logout } from "../lib/user";
-//import { auth } from '../lib/firebase.config';
+import $ from 'jquery';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+import Success from '../components/login/success';
+import Failed from '../components/login/failed';
 
 export default function Login() {
-  return <>
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <div class="card">
-                <div class="card-body">
-                    <form method="post" onSubmit={login}>
-                        <label for="email">Email Adresse</label>
-                        <input type="email" class="form-control" name="email" id="email" />
+    const loginAction = (e) => {
+        const username = $('#username').val();
+        const password = $('#password').val();
+
+        axios.post(
+            'http://127.0.0.1:43921/auth/login',
+            {
+                username: username,
+                password: password
+            }
+        ).then((res) => {
+            if (res.data.auth) {
+                const cookie = new Cookies();
+                cookie.set('UID', res.data.uid, { path: '/' });
+                cookie.set('TOKEN', res.data.token, { path: '/' });
+                if ($('#login-success-info').hasClass('hidden')) {
+                    $('#login-success-info').removeClass('hidden');
+                }
+            } else {
+                if ($('#login-failed-info').hasClass('hidden')) {
+                    $('#login-failed-info').removeClass('hidden');
+                }
+            }
+        });
+    };
+
+    return <>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <Success />
+                <Failed />
+
+                <div class="card">
+                    <div class="card-body">
+                        <label for="email">Benutzername</label>
+                        <input type="email" class="form-control" name="username" id="username" />
                         <label for="password">Passwort</label>
                         <input type="password" class="form-control" name="password" id="password" />
 
-                        <input type="submit" class="btn btn-outline-success" value="Login" />
-                    </form>
+                        <button class="btn btn-outline-success w-100" onClick={loginAction}>Login</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <button class="btn btn-danger" onClick={logout}>Logout</button>
-    </div>
-  </>;
+    </>;
 }
